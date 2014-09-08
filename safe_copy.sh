81bec009732249d67c52cbc922e3f9fabc84d3ce
Caleb Smith <caleb.smithnc@gmail.com>
2014-09-08 10:00:14 -0400

    Makes safe_copy create destination dir on first run

--- safe_copy.sh
+++ safe_copy.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
+# Checks if the source and target are different. Copies source to target
+# if so and stores a backup of the target in /backups
 safe_copy() {
-    # Checks if the source and target are different. Copies source to target
-    # if so and stores a backup of the target in /backups
+    # Check for destination directory and create it if needed
+    if ! [[ -d `dirname $2` ]] ; then
+        mkdir -p $(dirname $2)
+        cp $1 $2
+        cp $1 backups/
+        echo "Created " $2
+        safe_copy_result=0
+        exit
+    fi
+    # Determine if source and target are different
     `diff $1 $2 > /dev/null`
     if [ $? -ne 0 ] ; then
         cp $2 backups/
