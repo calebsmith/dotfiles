e2a777a114613ef6aade0a5a9484498bb930f331
Caleb Smith <caleb.smithnc@gmail.com>
2015-07-04 17:57:34 -0400

    Improvements to safe_copy.sh via shellcheck

--- safe_copy.sh
+++ safe_copy.sh
@@ -7,27 +7,27 @@ safe_copy() {
     if ! [[ -d backups ]] ; then
         mkdir backups
     fi
-    if ! [[ -d `dirname $2` ]] | [[ -f $2 ]] ; then
-        mkdir -p $(dirname $2)
-        cp $1 $2
-        cp $1 backups/
-        echo "Created " $2
+    if ! [[ -d $(dirname "$2") ]] | [[ -f $2 ]] ; then
+        mkdir -p "$(dirname "$2")"
+        cp "$1" "$2"
+        cp "$1" backups/
+        echo "Created " "$2"
         created=1
         safe_copy_result=0
     else
         created=0
     fi
     # Determine if source and target are different
-    `diff $1 $2 > /dev/null`
+    diff "$1" "$2" > /dev/null
     if [ $? -ne 0 ] ; then
-        cp $2 backups/
-        cp $1 $2
-        echo "Overwrote " $2 " with " $1 . "Use /backups for old version"
+        cp "$2" backups/
+        cp "$1" "$2"
+        echo "Overwrote " "$2" " with " "$1" . "Use /backups for old version"
         safe_copy_result=0
     else
         if [ $created -eq 0 ] ; then
-            echo $1 " is up to date. Skipping..."
-            safe_copy_result=1
+            echo "$1" " is up to date. Skipping..."
+            export safe_copy_result=1
         fi
     fi
 }
