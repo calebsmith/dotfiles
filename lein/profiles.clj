c89cdd4b645d522cbf813576ea487bb685eeaadc
Caleb Smith <caleb.smithnc@gmail.com>
2015-11-03 10:07:01 -0500

    Adds support for many clojure things

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,3 +1,11 @@
-{:user {:plugins [[cider/cider-nrepl "0.8.1"]
+{:user {:plugins [[cider/cider-nrepl "0.9.0-SNAPSHOT"]
+                  [refactor-nrepl "0.3.0-SNAPSHOT"]
                   [mvxcvi/whidbey "1.0.0"]
-                  [lein-kibit "0.1.2"]]}}
+                  [lein-kibit "0.1.2"]
+                  [lein-bin "0.3.5"]
+                  [lein-pprint "1.1.1"]
+                  [lein-deps-tree "0.1.2"]
+                  [debugger "0.1.7"]
+                  [jonase/eastwood "0.2.1"]]}
+ :dependencies [[alembic "0.3.2"]
+                [org.clojure/tools.nrepl "0.2.7"]]}
